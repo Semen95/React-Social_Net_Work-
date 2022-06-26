373377701aacf248356207e5019496348fc73c12
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-06-26 18:27:10 +0300

    Lesson-12

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.pricepony.com.ph/blog/wp-content/uploads/2018/07/company-name.jpg"/>
+        </header>
+    );
+}
+
+export default Header;
